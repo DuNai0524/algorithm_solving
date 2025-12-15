d711ce45e11ce7427d3f4390dff2c6f925882786
DuNai0524 <1094898402@qq.com>
2025-12-15 20:31:56 +0800

    update template

---
+++ templates/temp_codeforces.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+using u64 = unsigned long long;
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin>>T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
---
+++ templates/temp_others.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+using u64 = unsigned long long;
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+    return 0;
+}
